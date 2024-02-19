327dce2b7f7f41659b2d21189d3786492c0bfddb
Dmitry Kurgan <dimakurgan123789@gmail.com>
2024-02-19 19:01:20 +0200

    Changes for tests

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -51,20 +51,22 @@ export const validateAuthorization = [
         const [authType, token] = authHeader.split(' ');
 
         if (authType.toLowerCase() === 'basic') {
-            const authData = Buffer.from(token, 'base64').toString().split(':');
-            const username = authData[0];
-            const password = authData[1];
-
-            if (username !== 'admin' || password !== 'qwerty') {
-                return res.status(401).json({ message: 'Unauthorized: Invalid credentials' });
-            }
-        } else if (authType.toLowerCase() === 'bearer') {
-            // Проверяем, является ли токен правильным
+            // const authData = Buffer.from(token, 'base64').toString().split(':');
+            // const username = authData[0];
+            // const password = authData[1];
+            //
+            // if (username !== 'admin' || password !== 'qwerty') {
+            //     return res.status(401).json({ message: 'Unauthorized: Invalid credentials' });
+            // }
             if (token !== 'YWRtaW46cXdlcnR5') {
                 return res.status(401).json({ message: 'Unauthorized: Invalid Bearer token' });
             }
-        } else {
-            return res.status(401).json({ message: 'Unauthorized: Unsupported Authorization type' });
+        }
+
+        if (authType.toLowerCase() === 'bearer') {
+            if (token !== 'YWRtaW46cXdlcnR5') {
+                return res.status(401).json({ message: 'Unauthorized: Invalid Bearer token' });
+            }
         }
 
         next();
