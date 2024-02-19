21f5dc7c9424c38d1c24e3544e48bf5bdff4a2b5
Dmitry Kurgan <dimakurgan123789@gmail.com>
2024-02-19 02:42:31 +0200

    Changes for tests

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -4,14 +4,13 @@ import { body, validationResult } from 'express-validator';
 export const validateRequest = [
     body('name').isString().isLength({max:15}).withMessage('Invalid name value'),
     body('description').isString().isLength({max:500}).withMessage('Invalid description value'),
-    body('websiteUrl')
-        .isString().withMessage('Invalid websiteUrl value')
-        .isLength({max:100}).withMessage('Invalid websiteUrl length')
+    body('websiteUrl').isString().isLength({max:100})
+        .withMessage('Invalid websiteUrl value')
         .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
         .withMessage('Invalid websiteUrl pattern'),
 
-
     (req: Request, res: Response, next: NextFunction) => {
+        debugger
         // Проверка заголовка Authorization
         const authHeader = req.headers.authorization;
         if (!authHeader) {
