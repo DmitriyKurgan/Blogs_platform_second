b5661a33fb8580139259e71bac70f13f434d31de
Dmitry Kurgan <dimakurgan123789@gmail.com>
2024-02-24 00:10:04 +0200

    Changes for tests

--- src/middlewares/middlewares.ts
+++ src/middlewares/middlewares.ts
@@ -1,13 +1,49 @@
 import { Request, Response, NextFunction } from 'express';
 import {body, ValidationError, validationResult} from 'express-validator';
-
+const websiteUrlPattern =
+    /^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/;
 export const validateBlogsRequests = [
-    body('name').trim().isString().isLength({min: 1, max:15}).withMessage('name is too long'),
-    body('description').trim().isString().isLength({min: 1,max:500}).withMessage('description is too long'),
-    body('websiteUrl').trim().isString().isLength({min: 1,max:100})
-        .withMessage('website url is too long')
-        .matches(/^https:\/\/([a-zA-Z0-9_-]+\.)+[a-zA-Z0-9_-]+(\/[a-zA-Z0-9_-]+)*\/?$/)
-        .withMessage('website url does not match the template'),
+    body("name")
+        .exists()
+        .withMessage("Name is required")
+        .isString()
+        .withMessage("Type of name must be string")
+        .trim()
+        .isLength({
+            min: 1,
+            max: 15,
+        })
+        .withMessage(
+            "Name length must be more than 0 and less than or equal to 15 symbols"
+        ),
+    body("description")
+        .exists()
+        .withMessage("Description is required")
+        .isString()
+        .withMessage("Type of description must be string")
+        .trim()
+        .isLength({
+            min: 1,
+            max: 500,
+        })
+        .withMessage(
+            "Description length must be more than 0 and less than or equal to 500 symbols"
+        ),
+    body("websiteUrl")
+        .exists()
+        .withMessage("Website URL is required")
+        .isString()
+        .withMessage("Type of Website URL must be string")
+        .trim()
+        .isLength({
+            min: 1,
+            max: 100,
+        })
+        .withMessage(
+            "Website URL length must be more than 0 and less than or equal to 100 symbols"
+        )
+        .matches(websiteUrlPattern)
+        .withMessage("Website URL must be in correct format"),
 ];
 
 export const validatePostsRequests = [
